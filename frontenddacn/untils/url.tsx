40f7549853234fc07734c1601d4ccf85c01206b4
unknown <an.pd01114@sinhvien.hoasen.edu.vn>
2025-05-26 20:59:42 +0700

    sua format chart

--- frontenddacn/untils/url.tsx
+++ frontenddacn/untils/url.tsx
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const BASE_URL ="http://172.20.10.5:3000"
-=======
-export const BASE_URL ="http://192.168.2.148:3000"
->>>>>>> adf764f0b37f5cdbc495469b775b0c923c4097cd
+export const BASE_URL ="http://192.168.100.14:3000"
